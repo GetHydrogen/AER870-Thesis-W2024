fe769d831c8549004e01e5e1a562a67000d44d9f
GetHydrogen <146095585+GetHydrogen@users.noreply.github.com>
2024-04-12 01:53:10 -0400

    Code pre-testing

---
+++ Farneback (Dense).py
@@ -0,0 +1,71 @@
+import cv2 as cv
+import numpy as np
+import matplotlib.pyplot as plt
+import pyrealsense2 as rs
+
+#K = np.array([[385.5986022949219, 0.0, 326.3680419921875],
+#              [0.0, 385.1598815917969, 242.12353515625],
+#              [0.0, 0.0, 1.0]])
+fx = 385.5986022949219
+fy = 385.1598815917969
+fps = 30
+Z = 2.0
+
+pipeline = rs.pipeline()
+config = rs.config()
+config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
+
+pipeline.start(config)
+
+velocities_data = []
+
+try:
+    for _ in range(30):
+        pipeline.wait_for_frames()
+
+    frames = pipeline.wait_for_frames()
+    color_frame = frames.get_color_frame()
+    first_frame = np.asanyarray(color_frame.get_data())
+    prev_gray = cv.cvtColor(first_frame, cv.COLOR_BGR2GRAY)
+    mask = np.zeros_like(first_frame)
+    mask[..., 1] = 255
+
+    while True:
+        frames = pipeline.wait_for_frames()
+        color_frame = frames.get_color_frame()
+
+        if not color_frame:
+            continue
+
+        frame = np.asanyarray(color_frame.get_data())
+        gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+
+        flow = cv.calcOpticalFlowFarneback(prev_gray, gray, None, 0.5, 3, 15, 3, 5, 1.2, 0)
+        magnitude, angle = cv.cartToPolar(flow[..., 0], flow[..., 1])
+        mask[..., 0] = angle * 180 / np.pi / 2
+        mask[..., 2] = cv.normalize(magnitude, None, 0, 255, cv.NORM_MINMAX)
+        rgb = cv.cvtColor(mask, cv.COLOR_HSV2BGR)
+
+        ppf = np.mean(magnitude)
+        
+        px = (fx+fy) / 2
+        velocity = (ppf / px) * Z * fps
+
+        velocities_data.append(velocity)
+
+        cv.putText(rgb, f"Velocity: {velocity:.2f} m/s", (10, 30), cv.FONT_HERSHEY_SIMPLEX, 0.7, (255, 255, 255), 2, cv.LINE_AA)
+        cv.imshow('Dense Optical Flow', rgb)
+        
+        
+        prev_gray = gray
+
+        if cv.waitKey(1) & 0xFF == ord('q'):
+            break
+finally:
+    pipeline.stop()
+    cv.destroyAllWindows()
+    plt.figure()
+    plt.plot(velocities_data)
+    plt.title("Drone Velocity Scatters")
+    plt.ylabel("Velocity (m/s)")
+    plt.show()
---
+++ Lucas-Kanade (Sparse).py
@@ -0,0 +1,98 @@
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+import pyrealsense2 as rs
+
+velocities_data = []
+
+def run_tracking(pipeline, fx, fy, fps, Z):
+    global velocities_data
+
+    lk_hp = dict(winSize=(15, 15), maxLevel=2, criteria=(cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.03))
+    features_hp = dict(maxCorners=10, qualityLevel=0.3, minDistance=7, blockSize=7)
+    color = np.random.randint(0, 255, (10, 3))
+
+    while True:
+        first_frame = pipeline.wait_for_frames().get_color_frame()
+        if first_frame:
+            old_frame = np.asanyarray(first_frame.get_data())
+            old_gray = cv2.cvtColor(old_frame, cv2.COLOR_BGR2GRAY)
+            p0 = cv2.goodFeaturesToTrack(old_gray, mask=None, **features_hp)
+            if p0 is not None:
+                break
+
+    mask = np.zeros_like(old_frame)
+
+    while True:
+        frame = pipeline.wait_for_frames().get_color_frame()
+        if not frame:
+            continue
+
+        frame_image = np.asanyarray(frame.get_data())
+        frame_gray = cv2.cvtColor(frame_image, cv2.COLOR_BGR2GRAY)
+
+        p1, st, err = cv2.calcOpticalFlowPyrLK(old_gray, frame_gray, p0, None, **lk_hp)
+        if p1 is None or st is None:
+            raise TypeError("New references")
+
+        good_new = p1[st == 1]
+        good_old = p0[st == 1]
+
+        for i, (new, old) in enumerate(zip(good_new, good_old)):
+            a, b = new.ravel()
+            c, d = old.ravel()
+            mask = cv2.line(mask, (int(a), int(b)), (int(c), int(d)), color[i].tolist(), 2)
+            frame_image = cv2.circle(frame_image, (int(a), int(b)), 5, color[i].tolist(), -1)
+            
+            # Pixels per frame
+            dx_ppf = a - c 
+            dy_ppf = b - d
+            # Meters per frame
+            dx_mpf = (dx_ppf * Z) / fx
+            dy_mpf = (dy_ppf * Z) / fy
+            
+            velocity = fps * np.sqrt(dx_mpf**2 + dy_mpf**2)
+            velocities_data.append(velocity)
+            
+            cv2.putText(frame_image, f'Vel: {velocity:.2f} m/s', (int(a), int(b)), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1)
+
+        img = cv2.add(frame_image, mask)
+        cv2.imshow('Frame', img)
+        
+        
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+        old_gray = frame_gray.copy()
+        p0 = good_new.reshape(-1, 1, 2)
+
+
+def main():
+    fx = 385.5986022949219
+    fy = 385.1598815917969
+    fps = 30
+    Z = 2.0
+
+    pipeline = rs.pipeline()
+    config = rs.config()
+    config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
+    pipeline.start(config)
+
+    try:
+        while True:
+            try:
+                run_tracking(pipeline, fx, fy, fps, Z)
+            except TypeError as e:
+                print("Tracking feature out of frame,", e)
+    finally:
+        pipeline.stop()
+        cv2.destroyAllWindows()
+        print("Final velocities:", velocities_data)
+        plt.figure()
+        plt.plot(velocities_data)
+        plt.title("Drone Velocity Scatters")
+        plt.ylabel("Velocity (m/s)")
+        plt.show()
+        
+if __name__ == "__main__":
+    main()
